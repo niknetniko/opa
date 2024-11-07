388323463295028de0146ba6f44bff3015515dc9
Niko Strijbol <strijbol.niko@gmail.com>
2024-11-08 00:38:25 +0100

    Enable CMake formatter and format code
    Fixes #21

--- ecm_uninstall.cmake
+++ ecm_uninstall.cmake
@@ -1,20 +1,24 @@
 if(NOT EXISTS "/home/niko/Ontwikkeling/opa/install_manifest.txt")
-    message(FATAL_ERROR "Cannot find install manifest: /home/niko/Ontwikkeling/opa/install_manifest.txt")
+  message(
+    FATAL_ERROR
+      "Cannot find install manifest: /home/niko/Ontwikkeling/opa/install_manifest.txt"
+  )
 endif()
 
 file(READ "/home/niko/Ontwikkeling/opa/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
 foreach(file ${files})
-    message(STATUS "Uninstalling $ENV{DESTDIR}${file}")
-    if(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
-        execute_process(
-            COMMAND "/nix/store/yzi080r2c1zn2jzrhcfdv7dmr92yw07l-cmake-3.29.6/bin/cmake" -E remove "$ENV{DESTDIR}${file}"
-            RESULT_VARIABLE rm_retval
-            )
-        if(NOT "${rm_retval}" STREQUAL 0)
-            message(FATAL_ERROR "Problem when removing $ENV{DESTDIR}${file}")
-        endif()
-    else()
-        message(STATUS "File $ENV{DESTDIR}${file} does not exist.")
+  message(STATUS "Uninstalling $ENV{DESTDIR}${file}")
+  if(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+    execute_process(
+      COMMAND
+        "/nix/store/yzi080r2c1zn2jzrhcfdv7dmr92yw07l-cmake-3.29.6/bin/cmake" -E
+        remove "$ENV{DESTDIR}${file}"
+      RESULT_VARIABLE rm_retval)
+    if(NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing $ENV{DESTDIR}${file}")
     endif()
+  else()
+    message(STATUS "File $ENV{DESTDIR}${file} does not exist.")
+  endif()
 endforeach()
--- treefmt.nix
+++ treefmt.nix
@@ -4,7 +4,7 @@
   # Nix files
   programs.nixfmt-rfc-style.enable = true;
   # CMake files
-  programs.cmake-format.enable = false;
+  programs.cmake-format.enable = true;
   # C++
   programs.clang-format.enable = true;
   settings.formatter.clang-format.options = [
