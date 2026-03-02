e0b054797ae0bba02b2159c76f1e544871ebcba3
Niko Strijbol <strijbol.niko@gmail.com>
2026-03-02 21:25:12 +0100

    Add helper for transactions

--- src/database/database.h
+++ src/database/database.h
@@ -7,6 +7,8 @@
 
 #include <QLoggingCategory>
 // ReSharper disable once CppUnusedIncludeDirective
+#include <QSqlDatabase>
+#include <QSqlError>
 #include <QString>
 
 Q_DECLARE_LOGGING_CATEGORY(OPA_SQL);
@@ -22,3 +24,47 @@ void closeDatabase();
  * Return true if the database is currently in a transaction, false otherwise.
  */
 bool hasActiveTransaction();
+
+/**
+ * Execute a lambda in the context of a database-level transaction.
+ *
+ * If the lambda throws an exception, the transaction will be rolled back, the exception will be thrown.
+ *
+ * @param operation Returns false if it should abort, true otherwise.
+ * @return True if the operation was successful and committed, false otherwise.
+ */
+template <typename Function>
+auto executeInTransaction(const Function& operation) -> decltype(operation()) {
+    using ReturnType = decltype(operation());
+
+    auto db = QSqlDatabase::database();
+
+    if (!db.transaction()) {
+        qWarning() << "Failed to start transaction:" << db.lastError().text();
+        return std::nullopt;
+    }
+
+    try {
+        const ReturnType result = operation();
+        if (result.has_value()) {
+            if (db.commit()) {
+                return result;
+            } else {
+                qWarning() << "Failed to commit transaction:" << db.lastError().text();
+            }
+        }
+
+        if (!db.rollback()) {
+            qWarning() << "Failed to rollback transaction:" << db.lastError().text();
+        }
+
+        return std::nullopt;
+
+    } catch (...) {
+        if (!db.rollback()) {
+            qWarning() << "Failed to rollback transaction:" << db.lastError().text();
+        }
+
+        throw;
+    }
+}
